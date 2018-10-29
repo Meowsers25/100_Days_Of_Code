cad9770b4153ab6a75ef784171e575b1ef94aab3
Meowsers25 <c.lamothe25@gmail.com>
2018-10-28 22:40:52 -0400

    uncommented :)

--- day8.py
+++ day8.py
@@ -7,29 +7,29 @@ cars = {
 }
 grep = 'trail'
 
-# all_jeeps = ''
-# for keys, values in cars.items():
-#     if keys == 'Jeep':
-#         for i in range(len(values)):
-#             all_jeeps = all_jeeps + values[i] + ', '
-#         all_jeeps = all_jeeps[0:len(all_jeeps) -2]
-#         print(all_jeeps)
+all_jeeps = ''
+for keys, values in cars.items():
+    if keys == 'Jeep':
+        for i in range(len(values)):
+            all_jeeps = all_jeeps + values[i] + ', '
+        all_jeeps = all_jeeps[0:len(all_jeeps) -2]
+        print(all_jeeps)
 
-# cars_array = []
-# for keys, values in cars.items():
-#     cars_array.append(values[0])
-# print(cars_array)
+cars_array = []
+for keys, values in cars.items():
+    cars_array.append(values[0])
+print(cars_array)
 
 
 # import pdb;pdb.set_trace()
-# trail_cars = []
-# for keys, values in cars.items():
-#     for car in values:
-#         if grep.lower() in car.lower():
-#             print(car)    
-#             trail_cars.append(car)
-#             trail_cars.sort()
-# print(trail_cars)
+trail_cars = []
+for keys, values in cars.items():
+    for car in values:
+        if grep.lower() in car.lower():
+            print(car)    
+            trail_cars.append(car)
+            trail_cars.sort()
+print(trail_cars)
 
 # sd = sorted(cars.items())
 # for keys, values in sd:
